a14ea0f691b5cf95876934e7a8e31e78c2961d16
颜飞 <letteryan@qq.com>
2020-03-28 23:15:46 +0800

    darkMode

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import Index from "./pages/index";
@@ -8,7 +8,20 @@ import Themes from "./theme/themes";
 import "./app.less";
 
 function App() {
+  const [dark, setDark]: any = useState();
   const { theme } = useStore(sysConfig);
+  if (theme === "darkMode") {
+    if (dark) {
+      dark.disabled = false;
+    } else {
+      require("antd/dist/antd.dark.less");
+      const styleTags = document.getElementsByTagName("style");
+      const darkTag = styleTags[styleTags.length - 1];
+      if ((darkTag.type = "text/css")) setDark(darkTag);
+    }
+  } else if (dark) {
+    dark.disabled = true;
+  }
   return (
     <Router>
       <ThemeProvider theme={Themes[theme]}>
